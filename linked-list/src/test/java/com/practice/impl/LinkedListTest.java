d1f2ffc29c769387926a8ce611558c7b1ab61dac
Dmytro Horbachov <garbachhhhh21@gmail.com>
2021-05-13 12:55:13 +0300

    Added new methods

--- linked-list/src/test/java/com/practice/impl/LinkedListTest.java
+++ linked-list/src/test/java/com/practice/impl/LinkedListTest.java
@@ -2,7 +2,10 @@ package com.practice.impl;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class LinkedListTest {
 
@@ -51,5 +54,55 @@ public class LinkedListTest {
         list = MyLinkedList.of(123, 222);
         int lastElement = list.getLast();
         assertThat(lastElement).isEqualTo(222);
+    }
+
+    @Test
+    void getFirstOnEmptyList() {
+        assertThatExceptionOfType(NoSuchElementException.class)
+                .isThrownBy(() -> list.getFirst());
+    }
+
+    @Test
+    void getLastOnEmptyList() {
+        assertThatExceptionOfType(NoSuchElementException.class)
+                .isThrownBy(() -> list.getLast());
+    }
+
+    @Test
+    void getElements() {
+        list = MyLinkedList.of(1, 2, 3);
+        int firstElement = list.get(0);
+        int secondElement = list.get(1);
+        int thirdElement = list.get(2);
+
+        assertThat(firstElement).isEqualTo(1);
+        assertThat(secondElement).isEqualTo(2);
+        assertThat(thirdElement).isEqualTo(3);
+    }
+
+    @Test
+    void addElementByZeroIndexIntoEmptyList() {
+        list.add(0, 23);
+        int element = list.getFirst();
+        assertThat(element).isEqualTo(23);
+        assertThat(list.size()).isEqualTo(1);
+    }
+
+    @Test
+    void addElementByIndexToTheEndOfList() {
+        list = MyLinkedList.of(23, 321, 432, 54);
+        int indexForNewElement = list.size();
+        list.add(indexForNewElement, 123);
+        assertThat(list.get(indexForNewElement)).isEqualTo(123);
+        assertThat(list.size()).isEqualTo(5);
+    }
+
+    @Test
+    void addElementToTheHeadOfNonEmptyList() {
+        list = MyLinkedList.of(23, 321, 432, 54);
+        list.add(0, 99);
+        assertThat(list.get(0)).isEqualTo(99);
+        assertThat(list.get(1)).isEqualTo(23);
+        assertThat(list.size()).isEqualTo(5);
     }
 }
